058d4831585dfed5d65c92dbf5c40513cb5ef822
Lyg <2067798592@qq.com>
2023-04-13 13:46:35 +0800

    商城项目初始化

---
+++ shoppingmall-coupon/src/main/java/com/sasu/shoppingmall/shoppingmallcoupon/ShoppingmallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.sasu.shoppingmall.shoppingmallcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallCouponApplication.class, args);
+    }
+
+}
---
+++ shoppingmall-coupon/src/test/java/com/sasu/shoppingmall/shoppingmallcoupon/ShoppingmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasu.shoppingmall.shoppingmallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
